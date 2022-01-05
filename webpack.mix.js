de081f04b268888b588b2ee64aa79d15f11f1d7e
Daniel Bachhuber <daniel@bachhuber.co>
2022-01-05 05:35:45 -0800

    Prepare reproduction repo

---
+++ webpack.mix.js
@@ -0,0 +1,18 @@
+const mix = require('laravel-mix');
+
+require('laravel-mix-tailwind');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for your application, as well as bundling up your JS files.
+ |
+ */
+
+mix.sass('assets/scss/editor.scss', 'assets/dist/');
+
+mix.tailwind('./tailwind.config.js');
